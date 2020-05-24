764ddcfeb4b53f379e6a78169a3ead7a125d1bcd
vigenere23 <lolgab1@hotmail.com>
2020-05-24 18:18:26 -0400

    kind of implemented socket tests

--- nest-server/src/api/socket/gamesNamespace.ts
+++ nest-server/src/api/socket/gamesNamespace.ts
@@ -1,9 +1,11 @@
 import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets'
 import { PlayerInfos } from '../../domain/players/playerInfos'
 import { GameSystem } from '../../application/gameSystem'
+import { UsePipes, ValidationPipe } from '@nestjs/common'
 
 
 // TODO add validation
+@UsePipes(new ValidationPipe())
 @WebSocketGateway({ namespace: 'games' })
 export class GamesNamespace {
     constructor(
--- nest-server/tests/functionnal/socket/games.spec.ts
+++ nest-server/tests/functionnal/socket/games.spec.ts
@@ -1,10 +1,16 @@
-import { setup } from '../apiTest'
+import { setup, socket } from '../apiTest'
 
 
 setup('/games')
 
-describe('test', () => {
-    test('lol', () => {
-        expect(true).toBeTruthy()
+describe('games api', () => {
+    describe('when updating status with invalid payload', () => {
+        test('it returns an exception event', async done => {
+            socket.emit('updatePlayer', 'invalid payload')
+            socket.on('exception', (exception: any) => {
+                expect(exception.status).toEqual('error')
+                done()
+            })
+        })
     })
 })
